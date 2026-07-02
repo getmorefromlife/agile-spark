beb60ba9b0f5c45ef3569fcb7c3b2dcb8bdb33a1
getmorefromlife <59455269+getmorefromlife@users.noreply.github.com>
2026-07-02 17:52:49 +0500

    Register nitro plugin in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: { entry: "src/server.ts" },
-    }),
+    tanstackStart(),
+    nitro(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
